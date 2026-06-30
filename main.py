d68880cb64011bbd7a32034df5ffab34fe9337c9
1075678161 <c1075678161@gmail.com>
2026-06-30 09:48:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=46f7f4c2-1265-4587-9bcf-3772c932d697",
     # 添加更多链接
 ]
 
